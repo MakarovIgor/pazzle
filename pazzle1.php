99c3fe77d7ce2096c6ca78a0fec5a759cc240b78
igor <igormakarov1991@gmail.com>
2021-07-09 16:59:59 +0300

    finish

--- pazzle1.php
+++ pazzle1.php
@@ -12,18 +12,19 @@ function even_to_zero(int $number): int
     }
     return (int)implode($numbers);
 
-/*    var_dump((int)implode(array_map(function ($num, $index) {
-        var_dump($index, $num);
-        return ($index % 2) ? $index : 0;
-    }, str_split($number), array_keys(str_split($number)))));*/
+    /*    var_dump((int)implode(array_map(function ($num, $index) {
+            var_dump($index, $num);
+            return ($index % 2) ? $index : 0;
+        }, str_split($number), array_keys(str_split($number)))));*/
 }
 
 
 assert(is_palindrom("kayak") == true);
+assert(is_palindrom("шалаш") == true);
 
 function is_palindrom(string $word): string
 {
-    return $word == strrev($word);
+    return $word == strrev(join('', array_reverse(str_split($word))));
 }
 
 
@@ -31,9 +32,9 @@ assert(array_double([1, 2, 3]) == [2, 4, 6]);
 
 function array_double(array $intArray): array
 {
-    return array_map(function ($number) {
-        return $number * 2;
-    }, $intArray);
+    return array_map(fn($number) => $number * 2,
+        $intArray
+    );
 }
 
 
@@ -41,7 +42,6 @@ assert(only_odd([1, 2, 3]) == [1, 3]);
 
 function only_odd(array $intArray): array
 {
-    return array_values(array_filter($intArray, function ($number) {
-        return $number & 1;
-    }));
+    return array_values(array_filter($intArray, fn($number) => $number & 1
+    ));
 }
